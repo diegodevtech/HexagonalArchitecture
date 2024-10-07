48bea6475f13142bf440cf02ceef976231f39db3
diegodevtech <diegodevtechnology@gmail.com>
2024-10-07 15:32:35 -0400

    feat(db_product_test): Implements and tests DbProductTest_Get

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -11,18 +11,17 @@ type ProductDb struct {
 	db *sql.DB
 }
 
+func NewProductDb(db *sql.DB) *ProductDb{
+	return &ProductDb{db:db}
+}
+
 func (p *ProductDb) Get(id string) (application.ProductInterface, error){
 	var product application.Product
 	stmt, err := p.db.Prepare("select id, name, price, status from products where id=?")
 	if err != nil {
 		return nil, err
 	}
-	err = stmt.QueryRow(id).Scan(
-		&product.ID,
-		&product.Name,
-		&product.Price,
-		&product.Status,
-	)
+	err = stmt.QueryRow(id).Scan(&product.ID,&product.Name,&product.Price,&product.Status)
 	if err != nil {
 		return nil, err
 	}
