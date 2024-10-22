1d3d2e8ad1f70541d0fe97961ee03a5b907c2277
diegodevtech <diegodevtechnology@gmail.com>
2024-10-22 17:12:20 -0400

    feat: brings Cobra CLI and starts using service as CLI commands

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/diegodevtech/hexagonal-architecture/adapters/db"
-	"github.com/diegodevtech/hexagonal-architecture/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/diegodevtech/hexagonal-architecture/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3","db.sqlite")
-
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product Example", 30.0)
-	productService.Enable(product)
+	cmd.Execute()
 }
